129a0fe28218beead4dd08536932f8f53592765c
Cody Herriges <193064+ody@users.noreply.github.com>
2022-09-01 00:36:10 +0000

    Add a refresh task to sync state without apply
    Commit provides a task which executes a "terraform refresh" to enable
    the refreshment of state without running an apply, which could
    potentially destroy resources.
    
    Sometimes you'd rather capture changes to resources that were made
    external to the previous Terraform run, e.g. new public IP address after
    reboot or tag changes

---
+++ tasks/refresh.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative '../../ruby_task_helper/files/task_helper.rb'
+require_relative '../lib/cli_helper.rb'
+
+class TerraformRefresh < TaskHelper
+  def output(opts)
+    cli_opts = %w[-no-color -json]
+    dir = File.expand_path(opts[:dir]) if opts[:dir]
+    cli_opts << "-state=#{File.expand_path(opts[:state], dir)}" if opts[:state]
+    cli_opts = cli_opts.join(' ')
+
+    stdout_str, stderr_str, status = if dir
+                                       CliHelper.execute("terraform refresh #{cli_opts}", dir: dir)
+                                     else
+                                       CliHelper.execute("terraform refresh #{cli_opts}")
+                                     end
+    if status == 0
+      { 'stdout': stdout_str }
+    else
+      raise TaskHelper::Error.new(stderr_str, 'terraform/refresh-error')
+    end
+  end
+
+  def task(opts)
+    output(opts)
+  end
+end
+
+TerraformOutput.run if $PROGRAM_NAME == __FILE__
