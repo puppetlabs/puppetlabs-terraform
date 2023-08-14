dfca310edae82881d8a258c463d11e44c286e9f5
Jeffrey Clark <jeffrey.clark@perforce.com>
2023-08-13 22:04:56 -0500

    fix task spec tests
    failing tests due to _boltdir metaparameter is only available to
    plugins, like resolve_references

--- spec/tasks/terraform_output_spec.rb
+++ spec/tasks/terraform_output_spec.rb
@@ -11,11 +11,8 @@ describe TerraformOutput do
     let(:terraform_err) { "" }
     let(:terraform_code) { 0 }
     let(:terraform_response) { [terraform_out, terraform_err, terraform_code] }
-    let(:boltdir) { '/boltdir' }
-    let(:expected_dir) { File.join(boltdir, dir) }
-    let(:base_opts) { { _boltdir: boltdir } }
-    let(:additional_opts) { {} }
-    let(:opts) { base_opts.merge(additional_opts) }
+    let(:expected_dir) { File.join(Dir.pwd, dir) }
+    let(:opts) { {} }
     let(:success_result) { output }
     let(:base_cli) { %w[terraform output -no-color -json] }
     let(:additional_cli) { [] }
@@ -31,9 +28,8 @@ describe TerraformOutput do
 
     context "with dir option" do
       let(:dir) { "foo/bar" }
-      let(:additional_opts) { { dir: dir } }
-      it 'invokes terraform output with default args from dir relative to boltdir' do
-        expected_dir = File.expand_path(dir, boltdir)
+      let(:opts) { super().merge(dir: dir) }
+      it 'invokes terraform output with default args from dir relative to cwd' do
         expect(Open3).to receive(:capture3).with(cli, chdir: expected_dir).and_return(terraform_response)
         result = subject.output(opts)
         expect(result).to eq(success_result)
@@ -43,8 +39,8 @@ describe TerraformOutput do
     context "with state option" do
       let(:state) { "foo.tfstate" }
       let(:dir) { "foo/bar" }
-      let(:additional_opts) { { state: state, dir: dir } }
-      let(:additional_cli) { ["-state=#{File.join(boltdir, dir, state)}"] }
+      let(:opts) { super().merge(state: state, dir: dir) }
+      let(:additional_cli) { ["-state=#{File.expand_path(File.join(dir, state))}"] }
       it 'provides abosulute path for state relative to dir' do
         expect(Open3).to receive(:capture3).with(cli, chdir: expected_dir).and_return(terraform_response)
         result = subject.output(opts)
--- spec/tasks/terraform_refresh_spec.rb
+++ spec/tasks/terraform_refresh_spec.rb
@@ -10,11 +10,8 @@ describe TerraformRefresh do
     let(:terraform_err) { "" }
     let(:terraform_code) { 0 }
     let(:terraform_response) { [terraform_out, terraform_err, terraform_code] }
-    let(:boltdir) { '/boltdir' }
-    let(:expected_dir) { File.join(boltdir, dir) }
-    let(:base_opts) { { _boltdir: boltdir } }
-    let(:additional_opts) { {} }
-    let(:opts) { base_opts.merge(additional_opts) }
+    let(:expected_dir) { File.join(Dir.pwd, dir) }
+    let(:opts) { {} }
     let(:success_result) { { stdout: terraform_out } }
     let(:base_cli) { %w[terraform refresh -no-color] }
     let(:additional_cli) { [] }
@@ -30,9 +27,8 @@ describe TerraformRefresh do
 
     context "with dir option" do
       let(:dir) { "foo/bar" }
-      let(:additional_opts) { { dir: dir } }
-      it 'invokes terraform refresh with default args from dir relative to boltdir' do
-        expected_dir = File.expand_path(dir, boltdir)
+      let(:opts) { super().merge(dir: dir) }
+      it 'invokes terraform refresh with default args from dir relative to cwd' do
         expect(Open3).to receive(:capture3).with(cli, chdir: expected_dir).and_return(terraform_response)
         result = subject.output(opts)
         expect(result).to eq(success_result)
@@ -42,8 +38,8 @@ describe TerraformRefresh do
     context "with state option" do
       let(:state) { "foo.tfstate" }
       let(:dir) { "foo/bar" }
-      let(:additional_opts) { { state: state, dir: dir } }
-      let(:additional_cli) { ["-state=#{File.join(boltdir, dir, state)}"] }
+      let(:opts) { super().merge(state: state, dir: dir) }
+      let(:additional_cli) { ["-state=#{File.expand_path(File.join(dir, state))}"] }
       it 'provides abosulute path for state relative to dir' do
         expect(Open3).to receive(:capture3).with(cli, chdir: expected_dir).and_return(terraform_response)
         result = subject.output(opts)
